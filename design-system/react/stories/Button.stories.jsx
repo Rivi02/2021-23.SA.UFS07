337a5c73af0dc5cce7612f14594fd0f08d6c1f2d
Rivi02 <r.verdeiro@itsrizzoli.it>
2022-06-06 11:15:26 +0200

    feat: added buttons

--- design-system/react/stories/Button.stories.jsx
+++ design-system/react/stories/Button.stories.jsx
@@ -15,26 +15,21 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const Reset = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+Reset.args = {
+  label: 'Button Reset',
+  type: 'reset',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Submit = Template.bind({});
+Submit.args = {
+  label: 'Button Submit',
+  type: 'submit',
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Tasto = Template.bind({});
+Tasto.args = {
+  label: 'Button Tasto',
+  type: 'button'
 };
